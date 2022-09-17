91f5a65e25688c805b94c1dbef837001edf7f9c1
radizorit <jlimanalysis@gmail.com>
2022-09-16 17:42:33 -0700

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+
+function getHotel() {
+    const options = {
+        method: 'GET',
+        url: 'https://hotels4.p.rapidapi.com/locations/v2/search',
+        params: { query: 'new york', locale: 'en_US', currency: 'USD' },
+        headers: {
+            'X-RapidAPI-Key': '213bf8eeb6mshabac5e8f6740a32p17141djsnbf46e85640b0',
+            'X-RapidAPI-Host': 'hotels4.p.rapidapi.com'
+        }
+    };
+
