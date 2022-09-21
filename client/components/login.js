8eafb9f5c83ac5de3b6842e15c70cad05b31e914
Ian <ianhnizdo@gmail.com>
2022-09-21 12:52:04 -0400

    Renamed File

---
+++ client/components/login.js
@@ -0,0 +1,8 @@
+import { GoogleLogin } from 'react-google-login";
+
+const clientId= '830039597158-6nhs6p1u8eabg6k01r5qtnam1u1fa75q.apps.googleusercontent.com';
+const clientSecret = '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';
+
+// function Login() {
+//     return
+// }
